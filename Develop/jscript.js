b40554e81c9bc4faa472fa9d81afcbb92a2a78d6
majo <maria.riofrio.mr@gmail.com>
2020-04-04 23:38:47 -0400

    current work

--- Develop/jscript.js
+++ Develop/jscript.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
     displayBlocks()
+    // TO SHOW THE DATE AND TIME ON PAGE
     var todayDate = moment().format('dddd' + ", " + 'LL');
     document.getElementById("currentDay").innerHTML = todayDate;
+    // HOW TO ADD SHOW TEXT ON PAGE
     var saveComment = localStorage.getItem('9AM')
     $("#9AM").val(saveComment)
+    var saveComment = localStorage.getItem('10AM')
+    $("#10AM").val(saveComment)
+    var saveComment = localStorage.getItem('11AM')
+    $("#11AM").val(saveComment)
+    var saveComment = localStorage.getItem('12PM')
+    $("12PM").val(saveComment)
+    var saveComment = localStorage.getItem('1PM')
+    $("#1PM").val(saveComment)
+    var saveComment = localStorage.getItem('2PM')
+    $("#2PM").val(saveComment)
+    var saveComment = localStorage.getItem('3PM')
+    $("#3PM").val(saveComment)
+    var saveComment = localStorage.getItem('4PM')
+    $("#4PM").val(saveComment)
+    var saveComment = localStorage.getItem('5PM')
+    $("#5PM").val(saveComment)
 })
 // add rows/hr/text/button to page
 var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
@@ -21,15 +39,17 @@ function displayBlocks() {
         })
         markTimes();
     }
+    // SHOW TIMES AND TEX ON PAGE
     function markTimes() {
         let hours = $(".time-block")
         console.log(hours)
         hours.each(function (i, timeBlock) {
-            //console.log(timeBlock)// this thing is html element
-            decideTime($(this))// $(this) is the jquery element
+            
+            decideTime($(this))
         })
     }
-    const now = 13
+// HOW TO SHOW COLORS ON PAGE 
+    const now = times
     function decideTime(tB) {
         const htmlELEM = tB.children(".hour")[0]
         const hourNow = htmlELEM.innerHTML.split("")[0]
@@ -44,13 +64,16 @@ function displayBlocks() {
     }
     (tB.children("textarea")[0].classList.add(temporality))
 }
-// show hours in different times (past,present and future)
+// TO STORE ON LOCAL STORAGE 
 $(document).on("click", ".saveBtn", function () {
-    var comment = $(this).siblings('.description')
-    localStorage.setItem('9AM', comment.val())
+   
+    var value = $(this).siblings('.description').val();
+    var id = $(this).siblings('.description').attr('id');
+    localStorage.setItem(id, value)
 });
+
 
 
 // click fuction for save btn 
 //save info to local storage:
-//JSON.parse/stringify
+//JSON.parse
